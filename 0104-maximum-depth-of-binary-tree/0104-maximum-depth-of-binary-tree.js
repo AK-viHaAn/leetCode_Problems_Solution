1c551bbc5a0dcfcc615625d46c9e8288e2148da2
Anand Kumar <115540609+AK-viHaAn@users.noreply.github.com>
2023-04-01 20:49:32 +0530

    Time: 71 ms (56.80%), Space: 44.6 MB (72.32%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -11,15 +11,29 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    if(root==null){
-        return 0
-    }
-    if(root.left == null && root.right == null){
-        return 1
-    }
-    let highLeft = maxDepth(root.left)
-    let highRight = maxDepth(root.right)
-    let result = Math.max(highLeft,highRight)+1
-    return result
+//     if(root==null){
+//         return 0
+//     }
+//     if(root.left == null && root.right == null){
+//         return 1
+//     }
+//     let highLeft = maxDepth(root.left)
+//     let highRight = maxDepth(root.right)
+//     let result = Math.max(highLeft,highRight)+1
+//     return result
+    
+    
+    // second approach
+    
+       if(root == null)
+       return 0;
+   let left = maxDepth(root.left);
+   let right = maxDepth(root.right);
+
+
+   if(left> right)
+       return left+1;
+   else
+    return right+1;
     
 };
